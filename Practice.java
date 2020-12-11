b1433b5664302015b1ebeb37b3aed6e73c7e32b1
selcuk baris <selcuk.baris@treatwell.com>
2020-12-11 14:54:48 +0000

    adding another file

---
+++ Practice.java
@@ -0,0 +1,10 @@
+package CodingBat;
+
+public class Practice {
+
+    public static void main(String[] args) {
+        String tag = "<i>";
+        String word = "Yay";
+        System.out.println("<i>" + tag + "</i>" + ", " + "<i>" + word + "</i>");
+    }
+}
